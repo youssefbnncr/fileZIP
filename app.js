8f871a30244af994cdbd6d81c0a13d4d75d61dff
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-03-02 18:05:45 +0000

    basic setup without bootstrap

--- app.js
+++ app.js
@@ -9,20 +9,13 @@ const path = require('path');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.json());
 const assetsPath = path.join(__dirname, "src");
 app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret:  process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}));
-
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('home');
 })
 
 const port = process.env.PORT || 3000;
