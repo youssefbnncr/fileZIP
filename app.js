c9507c3677a33eeabc45f505ec0b5ddc0e70ec4c
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-03-03 12:44:11 +0000

    init tailwind

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ const path = require('path');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const assetsPath = path.join(__dirname, "src");
+const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
     res.render('home');
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
 })
 
 const port = process.env.PORT || 3000;
