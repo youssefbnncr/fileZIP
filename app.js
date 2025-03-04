023ca0a5fb0a2f915952b6fb52f73449ee0e77df
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-03-04 10:42:11 +0000

    add tailwind but its broken

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
+const passport = require("./config/passportConfig");
+require("dotenv").config();
 const path = require('path');
 
 const app = express();
@@ -14,32 +15,6 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-}));
-
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
-        return done(null, user);
-      });
-    }
-));
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    done(null,id);
-});
-
-app.use(passport.session())
 
 const router = require('./routes/routes.js');
 app.use('/', router);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./views/**/*.ejs", "./public/**/*.html", "./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50", // Green
+        dark: "#212121", // Almost Black
+        light: "#F5F5F5", // Off White
+        accent: "#2196F3", // Bright Blue
+        warning: "#FF5722", // Orange-Red
+      },
+    },
+  },
+  plugins: [],
+};
