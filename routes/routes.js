0f2379a71e95a946c2a51761aec854d198ef10e5
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-03-07 16:53:52 +0000

    handel all posible routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-  res.render('home');
-})
+const validPaths = ["bin", "media", "recent", "favorite", "cloud"];
 
-router.get('/signup',(req,res)=>{
-  res.render('signup')
-})
+router.get('/', (req, res) => {
+  res.render('home', { path: "cloud"});
+});
 
-router.get('/login',(req,res)=>{
-  res.render('login')
-})
+router.get('/:path', (req, res) => {
+  const { path } = req.params;
+  if (validPaths.includes(path)) {
+    res.render('home', { path });
+  } else {
+    res.render('page404');
+  }
+});
 
-router.get('/*',(req,res)=>{
-  res.render('page404');
-})
 module.exports = router;
