d8d59822a766b1ed0838f3e278ff17ca3b0df06a
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-03-09 14:39:21 +0000

    add users into databse

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const router = express.Router();
 
 const validPaths = ["bin", "media", "recent", "favorite", "cloud"];
 
+router.post('/signup',async (req,res)=>{
+  const {firstName, lastName, email, password} = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await prisma.users.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }
+  })
+  res.redirect('/login')
+})
+
+router.post('/login',(req,res)=>{
+
+})
 router.get('/', (req, res) => {
   res.render('home', { path: "cloud"});
 });
@@ -11,7 +33,7 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {const bcrypt = require('bcryptjs');
   res.render('login');
 });
 
